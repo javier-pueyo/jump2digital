6e0d6bede8c3644ae48fe4da453c3d31ad5f8de4
javier-pueyo <javier.pueyo@bootcamp-upgrade.com>
2022-11-07 21:10:10 +0100

    feat: Modal

--- src/components/PriceCard/PriceCard.jsx
+++ src/components/PriceCard/PriceCard.jsx
@@ -41,14 +41,16 @@ function PriceCard({ className, plan, features }) {
         </Button>
       </div>
 
-      <Dialog onClose={closeModal} open={isOpen}>
-          <Button className="secondary" handleClick={closeModal} >
-            <img width="16" height="16" src="/assets/img/icons/close.svg" alt="close" />
-          </Button>
-          <p class="cardPrice__name">You have selected:</p>
-          <p class="cardPrice__price">{name} Plan</p>
-          <p class="cardPrice__recurrence">Fill out this form to get more information about this plan.</p>
-          <Form/>
+      <Dialog className="modal" onClose={closeModal} open={isOpen}>
+          <div className="modal__content">
+            <Button className="modal__close" handleClick={closeModal} >
+              <img width="16" height="16" src="/assets/img/icons/close.svg" alt="close" />
+            </Button>
+            <p class="modal__subtitle cardPrice__name">You have selected:</p>
+            <p class="modal__title cardPrice__price">{name} Plan</p>
+            <p class="modal__description cardPrice__recurrence">Fill out this form to get more information about this plan.</p>
+            <Form/>
+          </div>
       </Dialog>
     </>
   );
